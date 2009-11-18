ab815d0f85d886d5272a47cc3868532de0a11238
marko <none@none>
2009-11-18 11:07:21 +0100

    added Mstage declaration

---
+++ src/mstage/model/MstageDeclaration.java
@@ -0,0 +1,53 @@
+package mstage.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rejuse.association.SingleAssociation;
+
+import chameleon.core.MetamodelException;
+import chameleon.core.declaration.Declaration;
+import chameleon.core.declaration.Signature;
+import chameleon.core.element.Element;
+import chameleon.core.lookup.LookupException;
+import chameleon.core.namespace.NamespaceElementImpl;
+import chameleon.core.scope.Scope;
+import chameleon.core.scope.UniversalScope;
+import chameleon.util.Util;
+
+public abstract class MstageDeclaration<E extends MstageDeclaration<E,P,S>, P extends Element<E,P>, S extends Signature> extends NamespaceElementImpl<E,P> implements Declaration<E,P,S,E> {
+
+	public E actualDeclaration() throws LookupException {
+		return (E) this;
+	}
+
+	public Scope scope() throws MetamodelException {
+		return new UniversalScope();
+	}
+
+	public Declaration<?, ?, ?, E> selectionDeclaration() throws LookupException {
+		return this;
+	}
+
+	public S signature() {
+		return _signature.getOtherEnd();
+	}
+	
+	private SingleAssociation<MstageDeclaration<E,P,S>, S> _signature = new SingleAssociation<MstageDeclaration<E,P,S>, S>(this); 
+
+	public List<Element> children() {
+		List<Element> result = new ArrayList<Element>();
+		Util.addNonNull(signature(), result);
+		return result;
+	}
+	
+	public void setSignature(S signature) {
+		if(signature != null) {
+			_signature.connectTo(signature.parentLink());
+		} else {
+			_signature.connectTo(null);
+		}
+	}
+
+
+}
