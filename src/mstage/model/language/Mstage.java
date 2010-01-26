cbc4698c1d2cbb879e5dd3ddeeac3e7fcc49579e
steveno <none@none>
2010-01-26 14:53:34 +0100

    mstage antlr parser and upstream compatibility fix

---
+++ src/mstage/model/language/Mstage.java
@@ -0,0 +1,49 @@
+/**
+ * author:   Steven Op de beeck <steven /at/ opdebeeck /./ org>
+ * filename: Mstage.java
+ * created:  Jan 22, 2010, 1:56:34 PM
+ * license:
+ * The code contained in this file is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * The code contained in this file is distributed in the hope that it 
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package mstage.model.language;
+
+import chameleon.core.language.Language;
+
+/**
+ * @author Steven Op de beeck <steven /at/ opdebeeck /./ org>
+ *
+ */
+public class Mstage extends Language {
+
+	/**
+	 * @param name
+	 */
+	public Mstage() {
+		super("Mstage");
+	}
+
+	@Override
+	protected void initializePropertyRules() {
+	}
+
+	@Override
+	protected void initializeValidityRules() {
+	}
+
+	@Override
+	public boolean isValidIdentifierCharacter(char character) {
+		return Character.isJavaIdentifierPart(character);
+	}
+
+}
