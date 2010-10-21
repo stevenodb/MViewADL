f6342f3978b03faa60076865f5c5d9d192774554
Steven Op de beeck <steven@opdebeeck.org>
2010-10-21 15:38:22 +0200

    introduced Actor Modifier properties

--- src/mview/model/language/MView.java
+++ src/mview/model/language/MView.java
@@ -18,10 +18,23 @@
  */
 package mview.model.language;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import mview.model.application.Application;
+import mview.model.application.Instance;
 import mview.model.composition.Pointcut;
+import mview.model.module.Component;
+import mview.model.module.Interface;
+import mview.reuse.Host;
+
+import org.rejuse.property.PropertyMutex;
+
+import chameleon.core.declaration.Declaration;
 import chameleon.core.declaration.SimpleNameSignature;
 import chameleon.core.language.Language;
 import chameleon.core.namespace.RootNamespace;
+import chameleon.core.property.ChameleonProperty;
 import chameleon.core.property.StaticChameleonProperty;
 
 /**
@@ -30,9 +43,23 @@ import chameleon.core.property.StaticChameleonProperty;
  */
 public class MView extends Language {
 
+
+	// mutex
+	public final PropertyMutex<ChameleonProperty> ACTOR_MUTEX;
+	
+	
 	// final. ... properties
 	public final StaticChameleonProperty EXECUTION;
 	public final StaticChameleonProperty CALL;
+	
+	public final StaticChameleonProperty INTERFACE;
+	public final StaticChameleonProperty COMPONENT;
+	public final StaticChameleonProperty APPLICATION;
+	public final StaticChameleonProperty INSTANCE;
+	public final StaticChameleonProperty HOST;
+	
+	private final Set<StaticChameleonProperty> ACTOR_PROPERTIES 
+		= new HashSet<StaticChameleonProperty>();
 	
 	/**
 	 * @param name
@@ -44,7 +71,43 @@ public class MView extends Language {
 		EXECUTION = new StaticChameleonProperty("Execution", this, Pointcut.class);
 		CALL = (StaticChameleonProperty) EXECUTION.inverse();
 		CALL.setName("Call");
+		
+		ACTOR_MUTEX = new PropertyMutex<ChameleonProperty>();
+		
+		INTERFACE = new StaticChameleonProperty("Interface", this, ACTOR_MUTEX, Interface.class);
+		ACTOR_PROPERTIES.add(INTERFACE);
+		
+		COMPONENT = new StaticChameleonProperty("Component", this, ACTOR_MUTEX, Component.class);
+		ACTOR_PROPERTIES.add(COMPONENT);
+		
+		INSTANCE = new StaticChameleonProperty("Instance",this,ACTOR_MUTEX,Instance.class);
+		ACTOR_PROPERTIES.add(INSTANCE);
+		
+		APPLICATION = new StaticChameleonProperty("Application", this, ACTOR_MUTEX, Application.class);
+		ACTOR_PROPERTIES.add(APPLICATION);
+		
+		HOST = new StaticChameleonProperty("Host", this, ACTOR_MUTEX, Host.class);
+		ACTOR_PROPERTIES.add(HOST);
+		
 	}
+	
+	
+	/**
+	 * @param declaration
+	 * @return	A PropertySet of actor properties for the given declaration
+	 */
+	public Set<ChameleonProperty> actorProperties(Class<Declaration> declaration) {
+		Set<ChameleonProperty> result = new HashSet<ChameleonProperty>();
+		
+		for (StaticChameleonProperty property : ACTOR_PROPERTIES) {
+			if (property.validElementTypes().contains(declaration)) {
+				result.add(property);
+			}
+		}
+		
+		return result;
+	}
+	
 
 	@Override
 	protected void initializePropertyRules() {
