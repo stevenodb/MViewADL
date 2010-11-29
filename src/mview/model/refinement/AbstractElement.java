7c4e76c638d8862726385a129382a8d112a67c51
Steven Op de beeck <steven@opdebeeck.org>
2010-11-29 16:30:20 +0100

    initial commit -- full behaviour isn't implemented yet

---
+++ src/mview/model/refinement/AbstractElement.java
@@ -0,0 +1,29 @@
+/**
+ * author:   Steven Op de beeck <steven /at/ opdebeeck /./ org>
+ * filename: AbstractElement.java
+ * created:  Nov 11, 2010, 5:10:50 PM
+ * license:
+ * The code contained in this file is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * The code contained in this file is distributed in the hope that it 
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package mview.model.refinement;
+
+/**
+ * @author Steven Op de beeck <steven /at/ opdebeeck /./ org>
+ *
+ */
+public interface AbstractElement {
+	
+	public boolean isAbstract();
+
+}
