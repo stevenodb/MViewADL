5cad9d4bf0dbe730ac5a7a537cc6ecab83c08cfa
Steven Op de beeck <steven@opdebeeck.org>
2010-12-22 10:46:51 +0100

    made it a type

--- src/mview/model/module/Interface.java
+++ src/mview/model/module/Interface.java
@@ -19,32 +19,16 @@ package mview.model.module;
 
 import java.util.List;
 
-import mview.model.namespace.MViewDeclaration;
-
 import org.rejuse.association.OrderedMultiAssociation;
 
-import chameleon.core.declaration.Declaration;
-import chameleon.core.declaration.DeclarationContainer;
-import chameleon.core.declaration.Signature;
 import chameleon.core.declaration.SimpleNameSignature;
-import chameleon.core.declaration.TargetDeclaration;
 import chameleon.core.element.Element;
-import chameleon.core.lookup.DeclarationSelector;
-import chameleon.core.lookup.LookupException;
-import chameleon.core.lookup.LookupStrategy;
 import chameleon.core.validation.BasicProblem;
 import chameleon.core.validation.VerificationResult;
+import chameleon.oo.type.RegularType;
 
-public class Interface extends MViewDeclaration<Interface, Element> 
-	/*implements 
-		TargetDeclaration<Interface, Element, Signature, Interface>,
-		DeclarationContainer<Interface, Element>*/{
-	
-	/**
-	 * Default constructor 
-	 */
-	protected Interface() {
-	}
+public class Interface extends RegularType {
+	// MViewDeclaration<Interface, Element> {
 
 	/**
 	 * @param signature
@@ -53,12 +37,12 @@ public class Interface extends MViewDeclaration<Interface, Element>
 		super(signature);
 	}
 
-	/* 
-	 * Association to Services 
+	/*
+	 * Association to Services
 	 */
-	private OrderedMultiAssociation<Interface, Service> _services = 
-		new OrderedMultiAssociation<Interface, Service>(this);
-	
+	private OrderedMultiAssociation<Interface, Service> _services =
+			new OrderedMultiAssociation<Interface, Service>(this);
+
 	/**
 	 * @param service
 	 */
@@ -74,7 +58,7 @@ public class Interface extends MViewDeclaration<Interface, Element>
 	public List<Service> services() {
 		return _services.getOtherEnds();
 	}
-	
+
 	/**
 	 * @param service
 	 */
@@ -84,110 +68,42 @@ public class Interface extends MViewDeclaration<Interface, Element>
 		}
 	}
 
-	
-	/* (non-Javadoc)
-	 * @see mview.model.namespace.MViewDeclaration#cloneThis()
-	 */
 	@Override
 	protected Interface cloneThis() {
-		return new Interface();
+		return new Interface(this.signature());
 	}
 
-	/* (non-Javadoc)
-	 * @see mview.model.namespace.MViewDeclaration#clone()
-	 */
 	@Override
 	public Interface clone() {
-		final Interface clone = super.clone();
-			
+		final Interface clone = (Interface) super.clone();
+
 		for (Service service : services()) {
 			Service localClone = service.clone();
-			
+
 			clone.addService(localClone);
 		}
-		
+
 		return clone;
 	}
 
-	/* (non-Javadoc)
-	 * @see mview.model.namespace.MViewDeclaration#verifySelf()
-	 */
 	@Override
 	public VerificationResult verifySelf() {
 		VerificationResult result = super.verifySelf();
-		
-		if ( ! (services() != null) ) {
+
+		if (!(services() != null)) {
 			result = result.and(new BasicProblem(this, "Services is null"));
 		}
-		
+
 		return result;
 	}
 
-	/* (non-Javadoc)
-	 * @see mview.model.namespace.MViewDeclaration#children()
-	 */
 	@Override
 	public List<Element> children() {
 		final List<Element> result = super.children();
-		
+
 		result.addAll(services());
-		
+
 		return result;
-		
+
 	}
-
-//	/* (non-Javadoc)
-//	 * @see chameleon.core.declaration.DeclarationContainer#declarations()
-//	 */
-//	@Override
-//	public List<? extends Declaration> declarations() throws LookupException {
-//		return locallyDeclaredDeclarations();
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see chameleon.core.declaration.DeclarationContainer#locallyDeclaredDeclarations()
-//	 */
-//	@Override
-//	public List<? extends Declaration> locallyDeclaredDeclarations()
-//			throws LookupException {
-//		
-//		return services();
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see chameleon.core.declaration.DeclarationContainer#declarations(chameleon.core.lookup.DeclarationSelector)
-//	 */
-//	@Override
-//	public <D extends Declaration> List<D> declarations(
-//			DeclarationSelector<D> selector) throws LookupException {
-//		return selector.selection(declarations());
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see chameleon.core.declaration.TargetDeclaration#targetContext()
-//	 */
-//	@Override
-//	public LookupStrategy targetContext() {
-//		return language().lookupFactory().createTargetLookupStrategy(this);
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see chameleon.core.element.ElementImpl#lexicalLookupStrategy(chameleon.core.element.Element)
-//	 */
-//	@Override
-//	public LookupStrategy lexicalLookupStrategy(Element child)
-//			throws LookupException {
-//		return lexicalMembersLookupStrategy();
-//	}
-//	
-//	private LookupStrategy lexicalMembersLookupStrategy() {
-//		if(_lus == null) {
-//			_lus = language().lookupFactory().createLexicalLookupStrategy(
-//					targetContext(), this);
-//		}
-//		return _lus;
-//	}
-//	
-//	private LookupStrategy _lus;
-
 }
