bf97b4a94a4ca17e585bdc57621640c030ec0045
Steven Op de beeck <steven@opdebeeck.org>
2014-03-24 14:31:59 +0100

    adding apache license header

--- src/mview/model/application/Host.java
+++ src/mview/model/application/Host.java
@@ -2,19 +2,24 @@
  * author:   Steven Op de beeck <steven /at/ opdebeeck /./ org>
  * filename: Host.java
  * created:  Jun 16, 2010, 12:19:08 PM
- * license:
- * The code contained in this file is free software: you can redistribute 
- * it and/or modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
  *
- * The code contained in this file is distributed in the hope that it 
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- * See the GNU General Public License for more details.
+ * Copyright 2013 KU Leuven Research and Development - iMinds - DistriNet
  *
- * You should have received a copy of the GNU General Public License. 
- * If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Administrative Contact: dnet-project-office@cs.kuleuven.be
+ * Technical Contact: steven@opdebeeck.org
  */
 package mview.model.application;
 
@@ -59,7 +64,7 @@ public class Host extends MViewMemberDeclarationImpl {
 	 *            the hostName to set
 	 */
 	public void setHostName(HostName hostName) {
-		set(_hostName,hostName);
+		set(_hostName, hostName);
 	}
 
 	/**
@@ -90,31 +95,33 @@ public class Host extends MViewMemberDeclarationImpl {
 		return result;
 	}
 
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see mview.model.refinement.MViewMember#overrides(mview.model.refinement.
-//	 * MViewMember)
-//	 */
-//	@Override
-//	public boolean overrides(Host other) {
-//		boolean equal = this.equals(other);
-//		boolean inARelationship =
-//				this.nearestAncestor(RefinableDeclaration.class).hasParent(
-//						other.nearestAncestor(RefinableDeclaration.class));
-//		return equal && inARelationship;
-//	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see
-//	 * mview.model.refinement.MViewMember#merge(mview.model.refinement.MViewMember
-//	 * )
-//	 */
-//	@Override
-//	public Host merge(Host member) throws MergeNotSupportedException {
-//		throw new MergeNotSupportedException(this + " doesn't support merge.");
-//	}
+	// /*
+	// * (non-Javadoc)
+	// *
+	// * @see
+	// mview.model.refinement.MViewMember#overrides(mview.model.refinement.
+	// * MViewMember)
+	// */
+	// @Override
+	// public boolean overrides(Host other) {
+	// boolean equal = this.equals(other);
+	// boolean inARelationship =
+	// this.nearestAncestor(RefinableDeclaration.class).hasParent(
+	// other.nearestAncestor(RefinableDeclaration.class));
+	// return equal && inARelationship;
+	// }
+	//
+	// /*
+	// * (non-Javadoc)
+	// *
+	// * @see
+	// *
+	// mview.model.refinement.MViewMember#merge(mview.model.refinement.MViewMember
+	// * )
+	// */
+	// @Override
+	// public Host merge(Host member) throws MergeNotSupportedException {
+	// throw new MergeNotSupportedException(this + " doesn't support merge.");
+	// }
 
 }
